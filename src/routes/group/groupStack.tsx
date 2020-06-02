8b6bf7eb3cde3d8371e9bdcc156ceba5a7eee453
Stanley <lims95@yahoo.com>
2020-06-02 16:24:16 -0400

    Added icons

--- src/routes/group/groupStack.tsx
+++ src/routes/group/groupStack.tsx
@@ -2,19 +2,49 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+// icons
+import { Ionicons } from "@expo/vector-icons";
+
 //screens 
-import Chat from '../screens/group/Chat';
-import Profile from '../screens/group/Profile';
-import Iou from '../screens/group/Iou';
+import Chat from '../../screens/group/Chat';
+import Profile from '../../screens/group/Profile';
+import Iou from '../../screens/group/Iou';
 
 // stacks
 import { PlannerStack } from './plannerStack';
 
-const Tab = createBottomTabNavigator();
+interface GroupTabsProps {};
+type GroupParamList = {
+  Planner: undefined;
+  Chat: undefined;
+  IOUs: undefined;
+  "My Profile": undefined;
+};
 
-const GroupStack = () => {
+const Tab = createBottomTabNavigator<GroupParamList>();
+const GroupStack: React.FC<GroupTabsProps> = ({}) => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        if (route.name === "Planner") {
+          return <Ionicons name={"ios-calendar"} size={size} color={color} />;
+        } 
+        else if (route.name === "Chat") {
+          return <Ionicons name={"ios-people"} size={size} color={color} />;
+        }
+        else if (route.name === "IOUs") {
+          // return <Ionicons name={"pay-circle1"} size={size} color={color} />;
+        }
+        else if (route.name === "My Profile") {
+          // return <Ionicons name={"profile"} size={size} color={color} />;
+        }
+      }
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray"
+      }}>
         <Tab.Screen name="Planner" component={PlannerStack} />
         <Tab.Screen name="Chat" component={Chat} />
         <Tab.Screen name="IOUs" component={Iou} />
