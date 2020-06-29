5fb0d9eb748981f5f230272f73072fa8ae89ff37
Joshua Buchman <joshua.buchman@msn.com>
2020-06-28 21:25:54 -0400

    another one (Stanley)

--- src/screens/group/AddEvent.tsx
+++ src/screens/group/AddEvent.tsx
@@ -4,10 +4,16 @@ import { CheckBox } from 'react-native-elements';
 import React, { useState, useEffect } from 'react';
 import { AxiosHttpRequest } from '../../utils/axios';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Dimensions, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Dimensions, AsyncStorage, ImageBackground, Image } from 'react-native';
 
 // components 
 import SetLocation from './SetLocation';
+
+// styles
+const bg = require('../../../assets/images/event.jpg');
+
+// icons
+import { AntDesign, FontAwesome, Entypo, Feather } from '@expo/vector-icons'; 
 
 const AddEvent = ({ navigation, route }: any) => {
     const [ name, setName ] = useState('');
@@ -67,51 +73,53 @@ const AddEvent = ({ navigation, route }: any) => {
     };
 
     return (
-        <SafeAreaView style={{ marginTop: 100, backgroundColor: '#b6d2dd' }}>
-            <TextInput 
-                style={styles.inputField} 
-                onChangeText={ text => setName(text) } 
-                value={ name } 
-                placeholder='Name of Event'
-            />
-            <TextInput 
-                style={styles.inputField} 
-                onChangeText={ text => setDescription(text) } 
-                value={ description } 
-                placeholder='Description'
-            />
+        <ImageBackground source={bg} style={styles.image}>
+            <SafeAreaView style={{ marginTop: 100}}>
+                <TextInput 
+                    style={styles.inputField} 
+                    onChangeText={ text => setName(text) } 
+                    value={ name } 
+                    placeholder='Name of Event'
+                />
+                <TextInput 
+                    style={styles.inputField} 
+                    onChangeText={ text => setDescription(text) } 
+                    value={ description } 
+                    placeholder='Description'
+                />
 
-            <Text>{moment(dateOfEvent).format('dddd, MMMM Do YYYY')}</Text>
-            <Button title="Set Date" onPress={ () => setDatePickerVisibility(true) } />
-            <DateTimePickerModal
-                isVisible={isDatePickerVisible}
-                mode="date"
-                onConfirm={handleDate}
-                onCancel={ () => setDatePickerVisibility(false) }
-            />
+                <Text>{moment(dateOfEvent).format('dddd, MMMM Do YYYY')}</Text>
+                <Button title="Set Date" onPress={ () => setDatePickerVisibility(true) } />
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleDate}
+                    onCancel={ () => setDatePickerVisibility(false) }
+                />
 
-            <Text>{moment(timeOfEvent).format('LT')}</Text>
-            <Button title="Set Time" onPress={ () => setTimePickerVisibility(true)} />
-            <DateTimePickerModal
-                isVisible={isTimePickerVisible}
-                mode="time"
-                onConfirm={handleTime}
-                onCancel={ () => setTimePickerVisibility(false) }
-            />
+                <Text>{moment(timeOfEvent).format('LT')}</Text>
+                <Button title="Set Time" onPress={ () => setTimePickerVisibility(true)} />
+                <DateTimePickerModal
+                    isVisible={isTimePickerVisible}
+                    mode="time"
+                    onConfirm={handleTime}
+                    onCancel={ () => setTimePickerVisibility(false) }
+                />
 
-            <Text>{addressOfEvent}</Text>
-            <Button title='Choose Location' onPress={ () => navigation.navigate('Set Location') } />
+                <Text>{addressOfEvent}</Text>
+                <Button title='Choose Location' onPress={ () => navigation.navigate('Set Location') } />
 
-            <CheckBox 
-                title='Public'
-                checked={isPublic}
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                onPress={ () => setPublic(!isPublic) }
-            />
+                <CheckBox 
+                    title='Public'
+                    checked={isPublic}
+                    checkedIcon='dot-circle-o'
+                    uncheckedIcon='circle-o'
+                    onPress={ () => setPublic(!isPublic) }
+                />
 
-            <Button title='Create event' onPress={ createEvent } disabled={ disabled } />
-        </SafeAreaView>
+                <Button title='Create event' onPress={ createEvent } disabled={ disabled } />
+            </SafeAreaView>
+        </ImageBackground>
     );
 };
 
